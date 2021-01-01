7aba767f381dc9afdbf4f9f0a142caaf80ef75b9
Lieberson  Xavier dos Santos <lieberson@lieberson.local>
2021-01-01 19:57:13 -0300

    finalizando projeto - deletando items

--- app/src/main/java/br/com/liebersonsantos/masteringrecyclerview/view/MainActivity.kt
+++ app/src/main/java/br/com/liebersonsantos/masteringrecyclerview/view/MainActivity.kt
@@ -1,9 +1,14 @@
 package br.com.liebersonsantos.masteringrecyclerview.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ItemTouchHelper.*
 import br.com.liebersonsantos.masteringrecyclerview.R
 import br.com.liebersonsantos.masteringrecyclerview.model.email
 import br.com.liebersonsantos.masteringrecyclerview.model.fakeEmails
@@ -15,6 +20,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mAdapter: EmailAdapter
+    private var actionMode: ActionMode? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         mAdapter = EmailAdapter(fakeEmails())
 
-        with(rvMain){
+        with(rvMain) {
             adapter = mAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
@@ -33,11 +39,66 @@ class MainActivity : AppCompatActivity() {
             rvMain.scrollToPosition(0)
         }
 
+        val helper = androidx.recyclerview.widget.ItemTouchHelper(
+            ItemTouchHelper(UP or DOWN, LEFT)
+        )
+
+        helper.attachToRecyclerView(rvMain)
+        mAdapter.onItemClick = {
+            enableActionMode(it)
+        }
+
+        mAdapter.onItemLongClick = {
+            enableActionMode(it)
+
+        }
 
     }
 
-    fun addEmail(){
-        val sdf =  SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")).parse(
+    private fun enableActionMode(position: Int) {
+        if (actionMode == null)
+            actionMode = startSupportActionMode(object : ActionMode.Callback {
+                override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                    mode?.menuInflater?.inflate(R.menu.menu_delete, menu)
+                    return true
+                }
+
+                override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                    return false
+                }
+
+                override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+                    if (item?.itemId == R.id.action_delete){
+                        mAdapter.deleteEmails()
+                        mode?.finish()
+                        return true
+                    }
+                    return false
+                }
+
+                override fun onDestroyActionMode(mode: ActionMode?) {
+                    mAdapter.selectedItems.clear()
+                    mAdapter.emails
+                        .filter { it.selected }
+                        .forEach { it.selected = false }
+                    mAdapter.notifyDataSetChanged()
+                    actionMode = null
+                }
+
+            })
+
+        mAdapter.toggleSelection(position)
+        val size = mAdapter.selectedItems.size()
+        if (size == 0) {
+            actionMode?.finish()
+        } else {
+            actionMode?.title = "$size"
+            actionMode?.invalidate()
+        }
+    }
+
+    private fun addEmail() {
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale("pt", "BR")).parse(
             Fakeit.dateTime().dateFormatter()
         )
 
@@ -48,12 +109,36 @@ class MainActivity : AppCompatActivity() {
             subject = Fakeit.company().name()
             date = SimpleDateFormat("d MMM", Locale("pt", "BR")).format(sdf)
             preview = mutableListOf<String>().apply {
-                repeat(10){
+                repeat(10) {
                     add(Fakeit.lorem().words())
                 }
             }.joinToString(" ")
         })
 
         mAdapter.notifyItemInserted(0)
+    }
+
+    inner class ItemTouchHelper(dragDirs: Int, swipeDirs: Int) :
+        androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback(dragDirs, swipeDirs) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            val from = viewHolder.adapterPosition
+            val to = target.adapterPosition
+
+            Collections.swap(mAdapter.emails, from, to)
+            mAdapter.notifyItemMoved(from, to)
+
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            mAdapter.emails.removeAt(viewHolder.adapterPosition)
+            mAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+
+        }
+
     }
 }
